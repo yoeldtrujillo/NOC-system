cd639c90ec4b28fae26fe0a70286613a66499d63
y0el22 <yoeldts@hotmail.com>
2023-12-20 00:21:36 -0300

    Se agregan variables de entorno y validaciones con env-var

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
+import { envs } from "./config/plugis/envs.plugins";
 import { Server } from "./presentation/server";
-
 
 (async () => {
   main();
 })();
 
 function main(){
-  Server.start();
+  // Server.start();
+  console.log(envs.PORT);
 }
